3028fb6fe77ec5650b9be3eed5417a53183eb577
pteskey <patrick.teskey@edu.sait.ca>
2021-03-24 16:24:02 -0600

    bug fix -- removed video playback speed

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,5 @@
 const menuToggle = document.querySelector('.toggle');
 const menu = document.querySelector('.menu');
-document.querySelector('video').playbackRate = 1;
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active')
