34abf5badfbb3999535c8980a13da01427348789
pteskey <patrick.teskey@edu.sait.ca>
2021-03-26 10:23:05 -0600

    added page titles to routes

--- routes/index.js
+++ routes/index.js
@@ -8,23 +8,23 @@ router.use((req, res, next) => {
 })
 //router for index page
 router.get('/', (req, res) => {
-  res.render('pages/index')
+  res.render('pages/index', {pageTitle: 'Home'})
 })
 //router for admin page
 router.get('/admin', (req, res) => {
-  res.render('pages/admin')
+  res.render('pages/admin', {pageTitle: 'Admin'})
 })
 //router for subscribe page
 router.get('/subscribe', (req, res) => {
-  res.render('pages/subscribe')
+  res.render('pages/subscribe', {pageTitle: 'Subscribe'})
 })
 //router for team page
 router.get('/team', (req, res) => {
-  res.render('pages/team')
+  res.render('pages/team', {pageTitle: 'Team'})
 })
 //router for gallery page
 router.get('/menu', (req, res) => {
-  res.render('pages/gallery')
+  res.render('pages/gallery', {pageTitle: 'Menu'})
 })
 //router for page not found page
 router.get('/404', (req, res) => {
