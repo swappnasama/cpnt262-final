c4bff13c2cb4755046505e126ebc097285f0c878
Swapna <sama.swapna@edu.sait.ca>
2021-03-26 11:28:13 -0600

    Teams page design including team seeds and single menu page

---
+++ seeds/team.js
@@ -0,0 +1,21 @@
+module.exports =[
+  {
+    name:"Liam" ,
+    profilePic:"images/liam.jpg" ,
+    title:"Executive Chef",
+    bio:"Calgary Based chef with extra-ordinary culinary skills",
+    
+  },
+  {
+    name:"Noah" ,
+    profilePic:"images/noah.jpg" ,
+    title:"Head Chef",
+    bio:"Toronto Based chef with extra-ordinary culinary skills",
+  },
+  {
+    name:"Alex" ,
+    profilePic: "images/alex.jpg",
+    title:"Deputy Chef",
+    bio:"Edmonton Based chef with extra-ordinary culinary skills",
+  }
+];
